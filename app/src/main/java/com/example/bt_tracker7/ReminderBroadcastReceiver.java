c3da92e22b29b68d0f86e66b3f3927ef069c0949
Sou-KIKUCHI <2020m047@kuas.ac.jp>
2020-06-25 22:11:07 +0900

    Class7_Task3  added log messages in BroadcastReceiver

--- app/src/main/java/com/example/bt_tracker7/ReminderBroadcastReceiver.java
+++ app/src/main/java/com/example/bt_tracker7/ReminderBroadcastReceiver.java
@@ -1,6 +1,7 @@
 package com.example.bt_tracker7;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -15,14 +16,24 @@ public class ReminderBroadcastReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // An Intent broadcast.
+
+        String TAG = "on Receive";
+
+        Intent notificationIntent = new Intent(context, LogActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context,0,notificationIntent, 0);
+
+        Log.d(TAG,"start building notification");
         NotificationCompat.Builder builder = new
-                NotificationCompat.Builder(context, "BT_Tracker_Channel")
+                NotificationCompat.Builder(context, "BTTrackerChannel")
                 .setSmallIcon(R.mipmap.ic_launcher_round)
                 .setContentTitle("Notification from BT Tracker")
                 .setContentText("Please log your body temperature now")
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT) ;
+        Log.d(TAG,"finish building notification");
         NotificationManagerCompat notificationManager =
                 NotificationManagerCompat.from(context) ;
+        Log.d(TAG,"start firing notification");
         notificationManager.notify(200, builder.build()); //ID could be any number
+        Log.d(TAG,"notification fired");
     }
 }
